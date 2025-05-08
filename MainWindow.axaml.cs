e35dfd64699dac7c66bf90a89c3c0f95a7d7b76a
iuniperum <julpietrzak147@gmail.com>
2025-05-08 11:00:18 +0200

    tabelki

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Interactivity;
 
 namespace baza;
 
@@ -7,5 +9,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void zatwierdzenie(object sender, RoutedEventArgs e)
+    {
+        
     }
 }
--- obj/Debug/net9.0/baza.AssemblyInfo.cs
+++ obj/Debug/net9.0/baza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("baza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b597bb0b5fba8b21a8fbac3f812b1a08f05599")]
 [assembly: System.Reflection.AssemblyProductAttribute("baza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("baza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
